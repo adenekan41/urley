f7c116c09ad77687ce1fe6e07398cb651c740740
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2020-05-15 05:13:50 +0100

    :rocket: (chore): formatted and deployed minor version

--- src/getBaseURL.js
+++ src/getBaseURL.js
@@ -3,18 +3,15 @@ import isBrowser from './isBrowser';
 /**
  * getBaseUrl definition
  * Returns the current URL without any parameters.
- * sets default parameter as the window current location
  * @param {String} url
  * @returns {String}
  */
 
 const getBaseUrl = (url = isBrowser() && window.location.href) => {
 	// try to check if url is passed
-	if (typeof url === 'string') return;
-	if (url.indexOf('?') > 0) {
-		return url.slice(0, url.indexOf('?'));
-	}
-	return url;
+	return url && typeof url === 'string' && url.indexOf('?') > 0
+		? url.slice(0, url.indexOf('?'))
+		: url;
 };
 
 export default getBaseUrl;
